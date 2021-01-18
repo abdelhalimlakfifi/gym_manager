fc296725f68270805a2806b93f7a5d45f2d9325a
BiLaLoViCc <77547073+BiLaLoViCc@users.noreply.github.com>
2021-01-18 22:58:07 +0100

    forme login

---
+++ fitness form/fitness form/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Username = "Admin" And Password = Root Then
+            MsgBox("Welcome ", MsgBoxStyle.Information, "Message")
+            Form1.Show()
+
+        Else
+            MsgBox("You enter a wrong username or password, please try again", MsgBoxStyle.Exclamation, "Warning")
+        End If
+    End Sub
+End Class
